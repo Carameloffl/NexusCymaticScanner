49f110876db2697bb43681e52888b3cf33f816e1
PAIN「ᴀᴋᴀᴛsᴜᴋɪ」 <81083883+PAINBOI2008@users.noreply.github.com>
2021-06-10 13:29:58 +0530

    Update __main__.py

--- Sibyl_System/__main__.py
+++ Sibyl_System/__main__.py
@@ -112,7 +112,7 @@ async def main():
             msg += f"\n**{plugin}**\n\n`{FAILED_TO_LOAD[plugin]}`"
         await System.send_message(Sibyl_logs, msg)
     else:
-        await System.send_message(Sibyl_logs, "I'm up!")
+        await System.send_message(Sibyl_logs, "Ready To Globally Ban Or Federation Ban Some Bad Peoples!")
     await System.run_until_disconnected()
 
 
